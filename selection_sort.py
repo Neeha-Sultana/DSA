24a05b5af2594c1b8b9430af44f1dfbcc8847524
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-03 09:34:55 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,9 +1,8 @@
-
 def selec_sort(lst1):
     for i in range(len(lst1)):
         sm = lst1[i]
         x = i
-        print("i=", i)
+
         for j in range(i+1, len(lst1)):
             if sm > lst1[j]:
                 sm = lst1[j]
@@ -12,8 +11,6 @@ def selec_sort(lst1):
         # Move the following two lines outside the inner loop
         lst1[x] = lst1[i]
         lst1[i] = sm
-        print(lst1)
-
     return lst1
 
 lst = [10, 7, 9, 4, 20, 8]
