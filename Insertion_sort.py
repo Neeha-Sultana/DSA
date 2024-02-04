cdd9838b9eec4bab9fa59e6bc65d681f03c951b5
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-04 20:43:04 +0530

    Create Insertion_sort.py

---
+++ Insertion_sort.py
@@ -0,0 +1,28 @@
+"""
+def bub_sort(lst1):
+    for i in range(1,len(lst1)):
+        for j in range(i,0,-1):
+            print("i=",i,"j=",j,"lst1=",lst1)
+            if lst1[j-1]>lst1[j]:
+                lst1[j],lst1[j-1]=lst1[j-1],lst1[j]
+            
+    return lst1
+
+lst = [10, 7, 9, 4, 20, 8]
+print(bub_sort(lst))
+"""
+
+
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while(j>=0 and arr[j]>key):
+            arr[j+1]=arr[j]
+            j=j-1
+        arr[j+1]=key
+    return arr
+arr=[10, 7, 9, 4, 20, 8]
+print(insertion_sort(arr))
+        
+
