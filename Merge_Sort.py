cc8bea2a195d5abe69e425d2fa3a781cb088be81
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-07 12:07:14 +0530

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,80 +1,35 @@
-# Python program for implementation of MergeSort
+def mergesort(arr,low,high):
+    med=(low+high)//2
+    if low==high:
+        return
+    mergesort(arr,low,med)
+    mergesort(arr,med+1,high)
+    merge(arr,low,med,high)
+    return arr
 
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
+def merge(arr,low,med,high):
+    x1=low
+    x2=med+1
+    tempp=[]
+    while(x1<=med and x2<=high):
+        if arr[x1]<=arr[x2]:
+            tempp.append(arr[x1])
+            x1+=1
+        else:
+            tempp.append(arr[x2])
+            x2+=1
 
+    while(x1<=med):
+        tempp.append(arr[x1])
+        x1+=1
+    while(x2<=high):
+        tempp.append(arr[x2])
+        x2+=1
 
-def merge(arr, l, m, r):
-	n1 = m - l + 1
-	n2 = r - m
-
-	# create temp arrays
-	L = [0] * (n1)
-	R = [0] * (n2)
-
-	# Copy data to temp arrays L[] and R[]
-	for i in range(0, n1):
-		L[i] = arr[l + i]
-
-	for j in range(0, n2):
-		R[j] = arr[m + 1 + j]
-
-	# Merge the temp arrays back into arr[l..r]
-	i = 0	 # Initial index of first subarray
-	j = 0	 # Initial index of second subarray
-	k = l	 # Initial index of merged subarray
-
-	while i < n1 and j < n2:
-		if L[i] <= R[j]:
-			arr[k] = L[i]
-			i += 1
-		else:
-			arr[k] = R[j]
-			j += 1
-		k += 1
-
-	# Copy the remaining elements of L[], if there
-	# are any
-	while i < n1:
-		arr[k] = L[i]
-		i += 1
-		k += 1
-
-	# Copy the remaining elements of R[], if there
-	# are any
-	while j < n2:
-		arr[k] = R[j]
-		j += 1
-		k += 1
-
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
-
-
-def mergeSort(arr, l, r):
-	if l < r:
-
-		# Same as (l+r)//2, but avoids overflow for
-		# large l and h
-		m = l+(r-l)//2
-
-		# Sort first and second halves
-		mergeSort(arr, l, m)
-		mergeSort(arr, m+1, r)
-		merge(arr, l, m, r)
-
-
-# Driver code to test above
-arr = [12, 11, 13, 5, 6, 7]
-n = len(arr)
-print("Given array is")
-for i in range(n):
-	print("%d" % arr[i],end=" ")
-
-mergeSort(arr, 0, n-1)
-print("\n\nSorted array is")
-for i in range(n):
-	print("%d" % arr[i],end=" ")
-
-# This code is contributed by Mohit Kumra
+    for i in range(low,high+1):
+        arr[i]=tempp[i-low]
+        
+ 
+print(mergesort([1,2,34,5,2,1,55,4],0,7))
+    
+            
