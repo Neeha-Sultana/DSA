8222d4ee83f99049c25015999882aa2965fb6b33
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-06 21:39:49 +0530

    Create Bubble_Sort.py

---
+++ Bubble_Sort.py
@@ -0,0 +1,9 @@
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+print(BubbleSort([1, 4, 3, 12, 98, 4]))
