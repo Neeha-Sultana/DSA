4b40d3e6047b93754cb2b55fd1db5a69db41c620
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-09 22:52:07 +0530

    Update Quick_Sort.py

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -16,7 +16,6 @@ def partition(arr, low, high):
             arr[i], arr[j] = arr[j], arr[i]
     arr[low], arr[j] = arr[j], arr[low]
     return j
-
 arr = [3, 56, 1, 54, 12, 4]
 qs(arr, 0, len(arr) - 1)
 print(arr)
