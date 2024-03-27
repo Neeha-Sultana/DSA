ca73209ec8b2e472b0b27926d3f31901bc419a51
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-27 23:34:50 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -46,7 +46,7 @@ class Graph:
                if neighbor not in visited:
                       self.dfsutil(neighbor,visited)
                             
-   
+
 gg=Graph()
 gg.addEdge(0,1)
 gg.addEdge(0,2)
