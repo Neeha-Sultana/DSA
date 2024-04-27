453ba0694c58fd4131f8625a4f13d6a0c84bafd7
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-04-27 23:58:57 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -15,7 +15,7 @@ def insertion_sort(arr):
         j=i-1
         while(j>=0 and arr[j]>key):
             arr[j+1]=arr[j]
-            
+    
             j=j-1
         arr[j+1]=key
     return arr
